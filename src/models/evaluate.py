ceef4f2e8ef3bbd2e39832967b58e191f9a095a7
Ludovic <ludovic.doucet@ik.me>
2025-05-01 17:13:51 +0000

    ajustements

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -4,12 +4,14 @@ from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 import numpy as np
 from src.tools.config import load_config
 import json
+import os
 
 # Paramètres
 data_config = load_config('./params.yaml')
 split_path = data_config['split_path']
 normalize_path = data_config['normalize_path']
 model_path = data_config['model_path']
+prediction_path = data_config['prediction_path']
 metrics_path = data_config['metrics_path']
 
 # Chargement du modèle
@@ -37,3 +39,8 @@ results = {
 
 with open(metrics_path + "scores.json", "w", encoding="utf-8") as f:
     json.dump(results, f, indent=4, ensure_ascii=False)
+
+# Sauvegarde des prédictions
+os.makedirs(os.path.dirname(prediction_path + "predictions.csv"), exist_ok=True)
+y_pred_df = pd.DataFrame(y_pred, columns=["predictions"])
+y_pred_df.to_csv(prediction_path + "predictions.csv", index=False)
